89d9d5ddb6e9842a9a487312fbdb570a201026de
Dykizz <phungphutai2005@gmail.com>
2025-11-11 17:57:10 +0700

    bỏ mã hóa

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,4 @@
-const WebpackObfuscator = require("webpack-obfuscator");
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer && process.env.NODE_ENV === "production") {
-      config.plugins.push(
-        new WebpackObfuscator(
-          {
-            rotateStringArray: true,
-            stringArray: true,
-            stringArrayEncoding: ["base64"],
-            stringArrayThreshold: 0.75,
-            deadCodeInjection: true,
-            deadCodeInjectionThreshold: 0.4,
-            identifierNamesGenerator: "hexadecimal",
-            unicodeEscapeSequence: false,
-          },
-          [
-            "feature.ts", // Obfuscate file này
-            "node_modules/**",
-            "**/*.tsx",
-          ]
-        )
-      );
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
