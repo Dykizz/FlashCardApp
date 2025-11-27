c4f6bd3ee96d7e9cf15a4973e2c2edec26c1fbf0
Dykizz <phungphutai2005@gmail.com>
2025-11-27 09:22:12 +0700

    sửa config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose", "@typegoose/typegoose"],
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig;
+export default nextConfig;
